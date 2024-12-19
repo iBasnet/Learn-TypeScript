bd182ffe38b97eeef79fef33697b1d1f8f9a70d4
Pratik Basnet <iampratikbasnet@gmail.com>
2024-12-19 21:22:48 +0545

    run the command tsc inside the src folder which creates an index.js file inside the dist folder

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const userData = [];
+const newData = {
+    name: 'pratik basnet',
+    age: 18,
+    password: 'GATA_ONLY.mp3',
+    isCoding: true
+};
+userData.push(newData);
+console.log(userData);
